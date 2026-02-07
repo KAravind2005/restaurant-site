5a3dc7bcd3662c46c87d72c6c8d0c3f3a406ad3f
KAravind2005 <kosanaaravind2005@gmail.com>
2026-02-07 23:38:26 +0530

    Fix MongoDB connection string

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,13 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/restaurantDB");
-
-    console.log("✅ MongoDB connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
   } catch (error) {
-    console.log("❌ DB connection error:", error);
+    console.log("DB connection error:", error);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
