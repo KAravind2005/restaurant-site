2269ea166fd82e05e68806ec60600010288d49c8
KAravind2005 <kosanaaravind2005@gmail.com>
2026-03-10 16:58:33 +0530

    Fix image static path

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-app.use("/images", express.static("images"));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 
 app.use("/api/auth", authRoutes);
